a5981e3e3535067efc32a0ca7e75f393714249d9
Kotsur Nazariy <kotsurnazariy@gmail.com>
2019-10-31 21:26:29 +0200

    add class work 1

---
+++ ClassWork1.py
@@ -0,0 +1,19 @@
+# a = 0
+# b = 100
+# while a < b:
+#     print (a)
+#     a += 2
+
+#print(list(range(0,100,2)))
+
+
+# for i in range(100):
+#     if i % 2 == 0:
+#         print(i)
+
+li = [2, 4, 6, 7, 8]
+while a == 
+
+# li = [1, 2, 3, 4, 5]
+# for i in li:
+#     print (float(i), end=' ')
