bbe732e276189a751f5f2a6896b07877fd24c33f
Kotsur Nazariy <kotsurnazariy@gmail.com>
2019-10-31 17:52:39 +0200

    add test2

--- test2.py
+++ test2.py
@@ -1,2 +1,4 @@
-four_integer = input ("Enter a four-digit positive integer: ")
-print (four_integer)
+four_int = [3, 5, 6, 8]
+print ('sum =', four_int[0] * four_int[1] * four_int[2] * four_int[3])
+
+print (four_int[-1], four_int[-2], four_int[-3], four_int[-4])
