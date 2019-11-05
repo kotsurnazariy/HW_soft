22c3a0e3ebe1fefb9d5092d9e73a5bb9da20de62
Kotsur Nazariy <kotsurnazariy@gmail.com>
2019-11-05 19:48:31 +0200

    add ClassWork2

---
+++ ClassWork2.py
@@ -0,0 +1,9 @@
+#1
+# how_much_num = [int(input('Enter list element: ')) for x in range(int(input ('Enter the number of numbers: ')))]
+# print (how_much_num)
+# print (max(how_much_num))
+# print (min(how_much_num))
+
+#2
+a = [x for x in range(1, 11)]
+print(a)
--- HW5.py
+++ HW5.py
@@ -1,5 +1,2 @@
 def number_to_string(num):
     return str(num)
-a = 10
-number_to_string (a)
-print (a == str)  #False???
