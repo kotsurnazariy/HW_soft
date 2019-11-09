f4d9d3aa72d4fee746f1ccdb51ec715da38661fc
Kotsur Nazariy <kotsurnazariy@gmail.com>
2019-11-09 22:54:39 +0200

    fix CW1

--- ClassWork1.py
+++ ClassWork1.py
@@ -1,19 +1,53 @@
+#1
 # a = 0
-# b = 100
-# while a < b:
+# while a < 100:
 #     print (a)
 #     a += 2
 
-#print(list(range(0,100,2)))
+# a = 0
+# for a in range(0, 100, 2):
+#     print(a)
 
+#2
+# a = 1
+# while a < 100:
+#     print(a)
+#     a += 2
 
-# for i in range(100):
-#     if i % 2 == 0:
-#         print(i)
+# a = 1
+# for a in range(100):
+#     if a % 2 == 0:
+#         continue
+#     print(a, end=' ')
 
-li = [2, 4, 6, 7, 8]
-while a == 
+#3
+# a = [2, 4, 6, 7, 10]
+# for a in a:
+#     if a % 2 == 1:
+#         print('This list have odd number', a)
+#         break
+#     else:
+#         print('This even number:', a)
 
-# li = [1, 2, 3, 4, 5]
-# for i in li:
-#     print (float(i), end=' ')
+#4
+# my_list = [1, 2, 3, 4, 5]
+# i = 0
+# for my_list[i] in my_list:
+#     b_len = float(my_list[i])
+#     print(b_len)
+#     i += 1
+
+#5
+# n = input("Enter any-think number: ")
+# n = int(n)
+# fib_1 = 0
+# fib_2 = 1
+# print(fib_1)
+# print(fib_2)
+# while fib_2 <= n:
+#     temp = fib_2
+#     fib_2 = fib_1 + fib_2
+#     fib_1 = temp
+#     print(temp)
+
+#6
--- ClassWork3.py
+++ ClassWork3.py
@@ -1,21 +1,20 @@
-
 #1
 
 #2
-def modul (b):
-    """Modul number"""
-    if b >= 0:
-        return b
-    else:
-        return -b
-print (modul(-10))
+# def modul (b):
+#     """Modul number"""
+#     if b >= 0:
+#         return b
+#     else:
+#         return -b
+# print (modul(-10))
 
 #3
-def max_num (g, k):
-    """
-    Print max number
-    """
-    return max(g, k)
-print (max_num(6, 8))
+# def max_num (g, k):
+#     """
+#     Print max number
+#     """
+#     return max(g, k)
+# print (max_num(6, 8))
 
 #4
