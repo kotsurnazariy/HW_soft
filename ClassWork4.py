a3cb6ee3f3fdef529b0e6c18b2769a0d38bdf755
Kotsur Nazariy <kotsurnazariy@gmail.com>
2019-11-12 23:00:16 +0200

    fix CW4

--- ClassWork4.py
+++ ClassWork4.py
@@ -13,3 +13,19 @@
 #         print ('try else')
 
 #2
+# import math
+# while True:
+#     user = int(input('What you want count? 1-Площа прямокутника, 2-Площа трикутника, 3-Площа кола 4-Вихід. Enter 1, 2, 3 or 4: '))
+#     if user == 1:
+#         x = int(input('Enter x='))
+#         y = int(input('Enter y='))
+#         print ('You result:', x*y)
+#     elif user == 2:
+#         h = int(input('Enter h='))
+#         a = int(input('Enter a='))
+#         print ('You result:', 0.5*h*a)
+#     elif user == 3:
+#         r = int(input('Enter r='))
+#         print ('You result:', math.pi*r*2)
+#     elif user == 4:
+#         break
