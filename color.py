dc990ed369dcfad112fa6f10484409792c555bdd
Kotsur Nazariy <kotsurnazariy@gmail.com>
2019-11-29 12:58:55 +0200

    add plane.py

---
+++ color.py
@@ -0,0 +1,23 @@
+import random
+class Ghost():
+    def __init__(self):
+    self.color = random.choice(['white', 'yellow', 'purple', 'red'])
+print(Ghost(color))
+
+# import random
+
+# class Ghost():
+#     # your code goes here
+#     def __init__(self):
+#         self.color = ['white', 'yellow', 'purple', 'red']
+#         return random.choice(self.color)
+# print(Ghost())
+
+
+
+# import random
+
+# class Ghost(object):
+#     # your code goes here
+#     def __init__(self):
+#         self.color = random.choice(['white', 'yellow', 'purple', 'red'])
